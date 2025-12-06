0e6f3a4cabad0047f10fcd819e7f2defbd932aec
andrew65411 <crakcelmejor@gmail.com>
2025-12-05 19:06:52 -0500

    fix(config): correct healt controller

--- src/main/java/com/chronicare/platform/HealthController.java
+++ src/main/java/com/chronicare/platform/HealthController.java
@@ -3,11 +3,11 @@ package com.chronicare.platform;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController("rootController")
 public class HealthController {
 
     @GetMapping("/")
     public String root() {
-        return "ChorniCare Backend is UP!";
+        return "ChroniCare Backend is running!";
     }
 }
